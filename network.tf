8d2882033d3d17e490d70869253c521193cdd9a8
sonyvegass <116088998+sonyvegass@users.noreply.github.com>
2024-11-18 22:03:58 +0100

    fourth commit

--- network.tf
+++ network.tf
@@ -5,7 +5,7 @@ resource "scaleway_vpc_private_network" "private-network" {
   ipv4_subnet {
     subnet = "10.0.0.0/22"
   }
-}
+} // celle-ci sera à supprimer
 
 resource "scaleway_vpc" "vpc" {
     name = "vpc-${var.prenom}"
